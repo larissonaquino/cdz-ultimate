9edfde3fbd3a4b0781d883f0ea411e706e681ec3
Lárisson Aquino <larissonaquino@gmail.com>
2021-04-09 03:21:22 -0300

    picpay links

--- src/app/views/cash/cash.component.ts
+++ src/app/views/cash/cash.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cash.component.css']
 })
 export class CashComponent implements OnInit {
+  urlBasePicPay: string = 'http://picpay.me/cdzultimate/'
+  picPayPrices: number[] = [10, 25, 50, 75, 100, 125, 150, 175, 200, 250, 300, 350, 400, 450, 500, 600, 700, 800, 900, 1000]
 
   constructor() { }
 
